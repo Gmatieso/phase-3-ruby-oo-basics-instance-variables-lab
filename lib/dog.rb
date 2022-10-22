8678ccc3834041fe7cf7fc9ffb318c097c22c86d
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-10-22 19:13:43 +0300

    :recycle: use instance variables

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,17 @@
 class Dog
+    #refactoring our dog class to use instance variables instead of local variable 
     #instance method to set dogs 
     def name=(dog_name)
-        this_dogs_name = dog_name
+        @this_dogs_name = dog_name
     end
 
     #instance method to get dogs name 
     def name
-        this_dogs_name
+        @this_dogs_name
     end
 end
 #creating instance of our class Dog 
 Lassie = Dog.new
 #give our dog a name 
 Lassie.name = "Lassie"
+puts Lassie.name
