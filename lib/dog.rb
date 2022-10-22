6846729d6161dc92869f30485c724ca4e6f67c4a
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-10-22 18:57:20 +0300

    :wrench: Dog instance method

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,16 @@
 class Dog
+    #instance method to set dogs 
+    def name = (dog_name)
+        this_dogs_name = dog_name
+    end
+
+    #instance method to get dogs name 
+    def name
+        this_dogs_name
+    end
 
 end
+#creating instance of our class Dog 
+lassie = Dog.new
+#give our dog a name 
+lassie.name = "Lassie"
